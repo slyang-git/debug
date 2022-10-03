d07b435440047f505bd3924b6ea4fbd4bda030f5
杨双龙 <slyang@aliyun.com>
2022-10-03 11:50:05 +0800

    Update

--- lib/lib.sh
+++ lib/lib.sh
@@ -48,34 +48,5 @@ Press_Start()
     stty ${OLDCONFIG}
 }
 
-usage() {
-cat 1>&2 <<EOF
-       __     __               
-  ____/ /__  / /_  __  ______ _
- / __  / _ \/ __ \/ / / / __  /
-/ /_/ /  __/ /_/ / /_/ / /_/ / 
-\__,_/\___/_.___/\__,_/\__, /  
-                      /____/   
-  
-
-debug is a tool for setup and running Go/PHP/Java/Python/C++ debugger.
-
-Usage:
-debug [flag] [command]
-
-[Flags]
--h, --help  Show this help message.
-
-Available Commands:
--------------------
-dlv      setup dlv debugger for Go. 
-xdebug   setup xdebug debugger for PHP. 
-upload   upload PHP source files to remote host  
-rsync    setup rsyc server for sync go binary file.
-dbgp     setup xdebug dbgpProxy for PHP.
-
-For more information, please visit: 
-EOF
-}
 
 CurrentIP=`ifconfig eth0 | grep inet | awk '{print $2}' | sed -e "s/addr://g"`
