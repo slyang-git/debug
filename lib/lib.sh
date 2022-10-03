b616c099cc2388bebc38031f4cdc4429a29025f2
杨双龙 <slyang@aliyun.com>
2022-10-03 10:45:58 +0800

    Add lib dir

---
+++ lib/lib.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+Color_Text()
+{
+  echo -e " \e[0;$2m$1\e[0m"
+}
+
+Echo_Red()
+{
+  echo $(Color_Text "$1" "31")
+}
+
+Echo_Green()
+{
+  echo $(Color_Text "$1" "32")
+}
+
+Echo_Yellow()
+{
+  echo $(Color_Text "$1" "33")
+}
+
+Echo_Blue()
+{
+  echo $(Color_Text "$1" "34")
+}
+
+
+Check_User()
+{
+    if [ `whoami` != "xiaoju" ]; then
+      Echo_Red ""
+      exit 1
+    fi
+}
+
+Press_Start()
+{
+    Echo_Green "👉 Press any key to start...or Press Ctrl+c to cancel"
+    OLDCONFIG=$(stty -g)
+    stty -icanon -echo min 1 time 0
+    dd count=1 2>/dev/null
+    stty ${OLDCONFIG}
+}
+
+CurrentIP=`ifconfig eth0 | grep inet | awk '{print $2}' | sed -e "s/addr://g"`
