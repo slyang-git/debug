9df88e559536cf502204229830f69b3da2dfd2d8
杨双龙 <slyang@aliyun.com>
2022-10-03 11:51:07 +0800

    Update

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 tmpPath="/tmp/debug"
 mkdir -p $tmpPath
 
-wget -crq https://raw.githubusercontent.com/slyang-git/debug/main/lib/lib.sh -O $tmpPath/lib.sh
+wget -rq https://raw.githubusercontent.com/slyang-git/debug/main/lib/lib.sh -O $tmpPath/lib.sh
 
 chmod u+x $tmpPath/*.sh
 source $tmpPath/*.sh
