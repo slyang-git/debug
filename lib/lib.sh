47ac0e74742ff0514bd393e5b4fcb8c1c4f4bc6c
杨双龙 <yangshuanglong@didiglobal.com>
2023-03-14 13:30:22 +0800

    Update

--- lib/lib.sh
+++ lib/lib.sh
@@ -7,6 +7,35 @@ Blue='\033[0;34m'
 Purple='\033[0;35m'
 NC='\033[0m'
 
+usage() {
+cat 1>&2 <<EOF
+       __     __               
+  ____/ /__  / /_  __  ______ _
+ / __  / _ \/ __ \/ / / / __  /
+/ /_/ /  __/ /_/ / /_/ / /_/ / 
+\__,_/\___/_.___/\__,_/\__, /  
+                      /____/   
+  
+
+debug is a tool for setup and running Go/PHP/Java/Python/C++ debugger.
+
+Usage:
+debug [flag] [command]
+
+[Flags]
+-h, --help  Show this help message.
+
+Available Commands:
+-------------------
+dlv      setup dlv debugger for Go. 
+xdebug   setup xdebug debugger for PHP. 
+upload   upload PHP source files to remote host  
+rsync    setup rsyc server for sync go binary file.
+dbgp     setup xdebug dbgpProxy for PHP.
+
+For more information, please visit: 
+EOF
+}
 
 OS_TYPE=''
 
